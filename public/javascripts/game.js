4393902b7b1f6e508d00e58bc4028a6b5fa4d093
Onfier <hmsluijter@gmail.com>
2018-12-06 17:27:51 +0100

    added check for pieces on bar

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -8,14 +8,40 @@ function onTurn(bool){
 
 function diceroll(){
   var rolls = [];
-  rolls[0] = Math.floor(Math.random * 6 + 1);
-  rolls[1] = Math.floor(Math.random * 6 + 1);
+  rolls[0] = Math.floor(Math.random() * 6 + 1);
+  rolls[1] = Math.floor(Math.random() * 6 + 1);
   return rolls;
 }
 
-function checkmoves(onTurn(true).positions, dice){
+function checkbar(player){
+  for (p in player.pieces){
+    if(p.position == 25){
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkfirstmoves(onTurn(true).positions, dice){
   second = onTurn(false);
   moves = [];
+
+  if(checkbar(onTurn(true))){
+    for(d in dice){
+      var move = 25 - d;
+      var count = 0;
+      for(i in second.pieces){
+        if(i.position == move){
+          count++;
+        }
+      }
+      if (count < 2){
+        moves.push([25, d]);
+      }
+    }
+    return moves;
+  }
+
   for(p in positions){
     for(d in dice){
       var move = p - d;
