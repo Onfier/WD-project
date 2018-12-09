1ac1f012502aff4f1ffa4cc53ded0f1a7e44290d
Onfier <hmsluijter@gmail.com>
2018-12-09 15:47:15 +0100

    move.execute

--- public/javascripts/move.js
+++ public/javascripts/move.js
@@ -1,8 +1,17 @@
 function Move(player, piecenumber, length){
-  this.oldpos = oldpos;
-  this.newpos = newpos;
+  this.player = player;
+  this.piecenumber = piecenumber;
+  this.length = length;
+  this.oldpos = player.positions[piecenumber];
+  this.newpos = oldpos - length;
 }
 
 Move.execute = function(){
-
+  player.positions[this.piecenumber] = this.newpos;
+  if (player.areWhite){
+    board.whitepostions[this.piecenumber] = this.newpos;
+  }
+  else{
+    board.blackpostions[this.piecenumber] += this.length;
+  }
 }
