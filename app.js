1cec5c54280201260b3308fc74ab14d8362b5b93
Onfier <hmsluijter@gmail.com>
2018-12-15 15:01:32 +0100

    first server client comm

--- app.js
+++ app.js
@@ -11,9 +11,112 @@ var port = process.argv[2];
 var app = express();
 
 app.use(express.static(__dirname + "/public"));
-http.createServer(app).listen(port);
+var server = http.createServer(app).;
+const wss = ws.Server(app);
+
+var websockets = {};
 
 app.get("/play", indexRouter);
 app.get("/", indexRouter);
+
+setInterval(function() {
+    for(let i in websockets){
+        if(websockets.hasOwnProperty(i)){
+            let gameObj = websockets[i];
+            //if the gameObj has a final status, the game is complete/aborted
+            if(gameObj.finalStatus!=null){
+                console.log("\tDeleting element "+i);
+                delete websockets[i];
+            }
+        }
+    }
+}, 50000);
+
+var currentGame = new Game(gameStatus);
+var connectionID = 0;//each websocket receives a unique ID
+
+wss.on("connection", function connection(ws) {
+
+    /*
+     * two-player game: every two players are added to the same game
+     */
+    let con = ws;
+    con.id = connectionID++;
+    let playerType = currentGame.addPlayer(con);
+    websockets[con.id] = currentGame;
+
+    console.log("Player %s placed in game %s as %s", con.id, currentGame.id, playerType);
+
+    /*
+     * inform the client about its assigned player type
+     */
+    con.send((playerType == "A") ? messages.S_PLAYER_A : messages.S_PLAYER_B);
+
+    /*
+     * once we have two players, there is no way back;
+     * a new game object is created;
+     * if a player now leaves, the game is aborted (player is not preplaced)
+     */
+    if (currentGame.hasTwoConnectedPlayers()) {
+        currentGame = new game(gameStatus.gamesInitialized++);
+    }
+
+    /*
+     * message coming in from a player:
+     *  1. determine the game object
+     *  2. determine the opposing player OP
+     *  3. send the message to OP
+     */
+    con.on("message", function incoming(message) {
+
+        let oMsg = JSON.parse(message);
+
+        let gameObj = websockets[con.id];
+        let isPlayerA = (gameObj.playerA == con) ? true : false;
+    });
+
+    con.on("close", function (code) {
+
+        /*
+         * code 1001 means almost always closing initiated by the client;
+         * source: https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent
+         */
+        console.log(con.id + " disconnected ...");
+
+        if (code == "1001") {
+            /*
+            * if possible, abort the game; if not, the game is already completed
+            */
+            let gameObj = websockets[con.id];
+
+            if (gameObj.isValidTransition(gameObj.gameState, "ABORTED")) {
+                gameObj.setStatus("ABORTED");
+                gameStatus.gamesAborted++;
+
+                /*
+                 * determine whose connection remains open;
+                 * close it
+                 */
+                try {
+                    gameObj.playerA.close();
+                    gameObj.playerA = null;
+                }
+                catch(e){
+                    console.log("Player A closing: "+ e);
+                }
+
+                try {
+                    gameObj.playerB.close();
+                    gameObj.playerB = null;
+                }
+                catch(e){
+                    console.log("Player B closing: " + e);
+                }
+            }
+
+        }
+    });
+});
+
 
 server.listen(port);
